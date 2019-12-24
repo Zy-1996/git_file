82888c62b325321302b3586e0b2785a976ad48a9
Zy-1996 <583163976@qq.com>
2019-12-24 20:44:54 +0800

    实现多线程多人抢票

--- weixin_test.py
+++ weixin_test.py
@@ -1,17 +1,17 @@
 import time
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
-from multiprocessing import  Process
+from threading import Thread
 
 
 class GrabVotes ():
 
-    def __init__(self, date, User_agent, url, name, id, proxy):
+    def __init__(self, date, User_agent, url, stu_name, stu_id, proxy):
         self.date = date
         self.User_agent = 'User-Agent='+User_agent
         self.url = url
-        self.name = name
-        self.id = id
+        self.name = stu_name
+        self.id = stu_id
         self.proxy = proxy
         self.browser = self.browser_start ()
 
@@ -55,16 +55,37 @@ class GrabVotes ():
 
                 break
 
+
+class mul_spider(Thread):
+
+    def __init__(self,grab_v):
+        super().__init__()
+        self.grab_v = grab_v
+
+    def run(self) :
+        self.grab_v.grab_votes()
+
+
 def main():
 
-    url = 'http://ndgiam1lsekpes40.mikecrm.com/EjwsjIi'
-    User_agent = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0Chrome/33.0.0.0 Mobile Safari/537.36 MicroMessenger/6.0.0.54_r849063.501 NetType/WIFI '
-    date = '2019-12-19 23:00:04'
-    name = '黄凯'
-    id = '2111904371'
+    url = 'http://dihuwfjvktlugpin.mikecrm.com/eLALQn0'
+    User_agent = ('Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0Chrome/33.0.0.0 Mobile Safari/537.36 MicroMessenger/6.0.0.54_r849063.501 NetType/WIFI ','mozilla/5.0 (iphone; cpu iphone os 5_1_1 like mac os x) applewebkit/534.46 (khtml, like gecko) mobile/9b206 micromessenger/5.0')
+    date = '2019-12-24 20:33:00'
+    name = ('黄凯','李新旺')
+    id = ('2111904371','2111904123')
     proxy = ''
-    A = GrabVotes ( date, User_agent, url, name, id, proxy )
-    A.grab_votes ()
+
+    spider_list = []
+
+    for i in range(len(name)) :
+        A = GrabVotes(date, User_agent[i], url, name[i], id[i], proxy)
+        t = mul_spider(A)
+        t.start()
+        spider_list.append(t)
+
+
+
+
 
 if __name__ == '__main__':
     main()
@@ -72,24 +93,3 @@ if __name__ == '__main__':
 
 
 
-"""过程"""
-# options = webdriver.ChromeOptions()
-# options.add_argument('User-Agent= mozilla/5.0 (linux; u; android 4.1.2; zh-cn; mi-one plus build/jzo54k) applewebkit/534.30 (khtml, like gecko) version/4.0 mobile safari/534.30 micromessenger/5.0.1.352  ')
-# browser = webdriver.Chrome(options = options)
-# browser.get('http://ndgiam1lsekpes40.mikecrm.com/EjwsjIi')
-#
-#
-# while True :
-#     locate_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime())
-#
-#     if locate_time == '2019-12-17 12:33:00':
-#         tag1 = browser.find_element_by_xpath ( '//div[@class="fbc_content fbc_widthCp"]/div/input' )
-#         tag2 = browser.find_element_by_xpath ( '//div[@class="fbc_imItem"]/div/input' )
-#         button = browser.find_element_by_id ( 'form_submit' )
-#         tag1.send_keys ( '李为撒' )
-#         tag2.send_keys ( '12321' )
-#         button.click ()
-#         print ( 'ok' )
-#         break
-#
-# browser.close ()
